bde27b15a2db14f5a44cd61395332f9e1cec3dc4
PoteznaSowa <64582266+PoteznaSowa@users.noreply.github.com>
2023-08-01 17:39:52 +0300

    Update Square.cs

--- 20_Lab5_1/Square.cs
+++ 20_Lab5_1/Square.cs
@@ -8,23 +8,18 @@ using System.Drawing.Drawing2D;
 namespace _20_Lab5_1 {
 	class Square {
 		protected float Pos_X {
-			// Координата X.
 			get; set;
 		}
 		protected float Pos_Y {
-			// Координата Y.
 			get; set;
 		}
 		protected float Size {
-			// Розмір сторони квадрата.
 			get; set;
 		}
 		protected Pen Outline {
-			// Контур квадрата.
 			get; set;
 		}
 		protected Brush FillBrush {
-			// Пензель для заливки.
 			get; set;
 		}
 
@@ -32,21 +27,18 @@ namespace _20_Lab5_1 {
 			Pos_X = 200.5F;
 			Pos_Y = 200.5F;
 
-			// Запросити у користувача розмір сторони і колір заливки.
 			Form2 f = new Form2();
 			if (f.ShowDialog() == DialogResult.OK) {
 				Size = f.ObjSize;
 				Outline = new Pen(Color.Black, 2);
 				FillBrush = new SolidBrush(f.ObjColour);
 			} else {
-				// Якщо це діалогове вікно закривається неправильно, закрити програму.
 				window.Close();
 			}
-			f.Dispose();  // Знищити діалогове вікно та звільнити пам'ять.
+			f.Dispose();
 		}
 
 		public virtual void ShowInfo() {
-			// Обчислити координати точок квадрата.
 			//
 			// A----B
 			// |    |
@@ -62,7 +54,6 @@ namespace _20_Lab5_1 {
 			float dx = Pos_X + Size - 1;
 			float dy = Pos_Y + Size - 1;
 
-			// Вивести на екран дані про цей квадрат.
 			MessageBox.Show(
 				$"Довжина сторони: {Size}\n" +
 				"Координати точок:\n" +
@@ -74,28 +65,28 @@ namespace _20_Lab5_1 {
 		}
 
 		public virtual void Draw(Graphics g, Color bgcolor) {
-			g.Clear(bgcolor);  // Очистити тло.
-			g.SmoothingMode = SmoothingMode.HighQuality;  // Зглажування.
-			g.DrawRectangle(Outline, Pos_X, Pos_Y, Size, Size);  // Намалювати контур квадрата.
-			g.FillRectangle(FillBrush, Pos_X, Pos_Y, Size, Size);  // Заливка.
+			g.Clear(bgcolor);
+			g.SmoothingMode = SmoothingMode.HighQuality;
+			g.DrawRectangle(Outline, Pos_X, Pos_Y, Size, Size);
+			g.FillRectangle(FillBrush, Pos_X, Pos_Y, Size, Size);
 		}
 
 		public virtual bool MoveLeft(Form window) {
-			if (Pos_X > 0) {  // Перевірити, що квадрат не вилазить за межі форми.
+			if (Pos_X > 0) {
 				Pos_X--;
-				window.Invalidate();  // Перемалювати квадрат.
-				return true;  // Успішно.
+				window.Invalidate();
+				return true;
 			}
-			return false;  // Невдало.
+			return false;
 		}
 
 		public virtual bool MoveRight(Form window) {
-			if (Pos_X < window.Width - Size - 17) {  // Перевірити, що квадрат не вилазить за межі форми.
+			if (Pos_X < window.Width - Size - 17) {
 				Pos_X++;
-				window.Invalidate();  // Перемалювати квадрат.
-				return true;  // Успішно.
+				window.Invalidate();
+				return true;
 			}
-			return false;  // Невдало.
+			return false;
 		}
 	}
 }
